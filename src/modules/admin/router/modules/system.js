edafe410f2559b1d613c9f4ba582e0719ba726ab
dong <390149386@qq.com>
2019-03-20 10:06:15 +0800

    优化

--- src/modules/admin/router/modules/system.js
+++ src/modules/admin/router/modules/system.js
@@ -3,9 +3,9 @@ const _import = require('./../_import_' + process.env.NODE_ENV);
 // 配置管理
 export const backgroundAccountManagement =
 {
-  path: 'accountManagement',
+  path: 'backgroundAccountManagement',
   component: _import('backgroundAccountManagement/index'),
-  redirect: '/product/accountManagement/account',
+  redirect: '/product/backgroundAccountManagement/account',
   name: '后台账户管理',
   meta: { title: '后台账户管理', icon: '我的协议' },
   children: [
